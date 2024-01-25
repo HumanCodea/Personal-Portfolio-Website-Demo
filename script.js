d828046b089df8bac06ecdee2b4f4e04216640d2
Nguyen Quy Than <quythan306@gmail.com>
2024-01-25 16:13:57 +0700

    Demo Website

---
+++ script.js
@@ -0,0 +1,24 @@
+// Ham tu dong chuyen den element
+function scrollToElement(elementSelector, instance = 0){
+    const elements = document.querySelectorAll(elementSelector);
+    if(elements.length > instance){
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+// Xu li js khi click
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener('click', () =>{
+    scrollToElement('.header');
+})
+
+link2.addEventListener('click', () =>{
+    scrollToElement('.header', 1);
+})
+
+link3.addEventListener('click', () =>{
+    scrollToElement('.column');
+})
